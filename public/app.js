dc7d7fa765421f5251b5aaf9607e7941204f8544
Aneeq Hassan <aneeqh0345@gmail.com>
2025-06-30 12:10:28 +0500

    init

---
+++ public/app.js
@@ -0,0 +1,38 @@
+function App(){
+    return {
+        index: 0,
+        params: {cyear: '',csemester: '',cfaculty: ''}, 
+        years:[],
+        async getyears(){
+            const years = await fetch(`/api/years`).then((res)=>res.json());
+            this.years = years;
+        },
+
+        semesters:[],
+        async getsemesters(year){
+            this.params.csemester = '';
+            this.params.cfaculty = '';
+            this.faculty = [];
+            this.grades = [];
+            this.params['cyear'] = year; 
+            const semesters = await fetch(`/api/semesters/${this.params.cyear}`).then((res)=>res.json());
+            this.semesters = semesters;
+        },
+
+        faculty:[],
+        async getfaculty(semester){
+            this.params.cfaculty = '';
+            this.grades = [];
+            this.params['csemester'] = semester;
+            const faculty = await fetch(`/api/faculty/${this.params.cyear}/${this.params.csemester}`).then((res)=>res.json());
+            this.faculty = faculty;
+        },
+        grades:[],
+        async getgrades(faculty){
+            this.params['cfaculty'] = faculty;
+            const grade = await fetch(`/api/grades/${this.params.cyear}/${this.params.csemester}/${this.params.cfaculty}`).then((res)=>res.json());
+            this.grades = grade;
+
+        }
+    }
+} 
